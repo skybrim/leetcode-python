eaf92739aa9258ec2e8253d980fce7f73a649d93
skybrim <throughskybrim@gmail.com>
2020-03-11 13:57:51 +0800

    median_finder

--- top_interview_questions_in_2018/stack_heap_queue/median_finder.py
+++ top_interview_questions_in_2018/stack_heap_queue/median_finder.py
@@ -4,25 +4,49 @@
 # @Author  : Wiley
 # @Email   : throughskybrim@gmail.com
 # @File    : median_finder.py
+"""
+MedianFinder
+"""
+import heapq
 
 
 class MedianFinder:
-
+    """
+    MedianFinder
+    """
     def __init__(self):
         """
         initialize your data structure here.
         """
-        self.array = []
+        self.max_heap = []
+        self.min_heap = []
+        self.count = 0
 
-    def add_num(self, num: int) -> None:
-        self.array.append(num)
+    def add_num(self, num):
+        """
+        add num
+        """
+        self.count += 1
+        heapq.heappush(self.max_heap, -num)
+        max_heap_top = heapq.heappop(self.max_heap)
+        heapq.heappush(self.min_heap, -max_heap_top)
+        if self.count & 1:
+            min_heap_top = heapq.heappop(self.min_heap)
+            heapq.heappush(self.max_heap, -min_heap_top)
 
-    def find_median(self) -> float:
-        count = len(self.array)
-        if count == 0:
-            return 0
-        self.array.sort()
-        if (count & 1) == 0:
-            return (self.array[count//2] + self.array[count//2-1])/2
-        else:
-            return self.array[count//2]
+    def find_median(self):
+        """
+        find media
+        """
+        if self.count & 1:
+            return -self.max_heap[0]
+        return (-self.max_heap[0] + self.min_heap[0]) / 2
+
+
+if __name__ == "__main__":
+    OBJ = MedianFinder()
+    OBJ.add_num(1)
+    OBJ.add_num(2)
+    print(OBJ.find_median())
+    OBJ.add_num(3)
+    print(OBJ.find_median())
