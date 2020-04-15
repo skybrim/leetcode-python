4a73a1a94201a65631e3333eebc52b9c67c7d940
skybrim <throughskybrim@gmail.com>
2020-04-15 10:42:33 +0800

    count_primes

--- top_interview_questions_in_2018/bitwise_operation/count_primes.py
+++ top_interview_questions_in_2018/bitwise_operation/count_primes.py
@@ -13,18 +13,18 @@ def count_primes(n):
     :type n: int
     :rtype int
     """
+    if n < 2:
+        return 0
     # 创建全是 true 的数组，记录当前 index 的数字是否是素数
     store = [1] * n
-    # 统计有多少个素数
-    count = 0
+    store[0], store[1] = 0, 0
     # 遍历
-    for i in range(2, n):
+    for i in range(2, int(n ** 0.5) + 1):
         # 如果当前数字是素数，那么它的倍数都不是素数
         if store[i] == 1:
-            count += 1
             for j in range(i*i, n, i):
                 store[j] = 0
-    return count
+    return sum(store)
 
 
 if __name__ == '__main__':
