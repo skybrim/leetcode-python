a7e88ac170c9ee1050dbee744af179ff92549202
skybrim <throughskybrim@gmail.com>
2020-10-19 09:59:44 +0800

    everyday backspace_compare

---
+++ everyday/20201019_844_backspace_compare.py
@@ -0,0 +1,49 @@
+"""
+844. 比较含退格的字符串
+
+给定 S 和 T 两个字符串，当它们分别被输入到空白的文本编辑器后，判断二者是否相等，并返回结果。 # 代表退格字符。
+注意：如果对空文本输入退格字符，文本继续为空。
+提示：
+1 <= S.length <= 200
+1 <= T.length <= 200
+S 和 T 只含有小写字母以及字符 '#'。
+
+示例 1：
+输入：S = "ab#c", T = "ad#c"
+输出：true
+解释：S 和 T 都会变成 “ac”。
+
+示例 2：
+输入：S = "ab##", T = "c#d#"
+输出：true
+解释：S 和 T 都会变成 “”。
+
+示例 3：
+输入：S = "a##c", T = "#a#c"
+输出：true
+解释：S 和 T 都会变成 “c”。
+
+示例 4：
+输入：S = "a#c", T = "b"
+输出：false
+解释：S 会变成 “c”，但 T 仍然是 “b”。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/backspace-string-compare
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+def backspace_compare(S, T):
+    stack_s, stack_t = [], []
+    for char in S:
+        if char == '#' and stack_s:
+            stack_s.pop()
+        elif char != '#':
+            stack_s.append(char)
+    for char in T:
+        if char == '#' and stack_t:
+            stack_t.pop()
+        elif char != '#':
+            stack_t.append(char)
+    return ''.join(stack_s) == ''.join(stack_t)
